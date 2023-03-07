0e5b2b324beb0b37d06c66e16f7f562029d0eadc
Amanda <amanda.costa1301@hotmail.com>
2023-03-07 11:43:16 -0300

    models/serializers

--- Copies/serializers.py
+++ Copies/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from Books.serializers import BookSerializer
 from Copies.models import Copy
+from Users.serializers import UserSerializer
 
 class CopySerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     book = BookSerializer()
-    total_amount =  serializers.DateTimeField(auto_now_add=True)
-    borrow_amount =  serializers.DateTimeField(auto_now_add=True)
+    total_amount =  serializers.IntegerField()
+    borrow_amount =  serializers.IntegerField()
+
+class BorrowSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    borrow_date = serializers.DateTimeField(auto_now_add=True)
+    return_date = serializers.DateTimeField(auto_now_add=True)
+    user = UserSerializer()
+    copy = CopySerializer()
