5d26fa84e372429165c4e9fe4dea2175ab7d5074
Vinicius Pacheco <viniciusgrp@gmail.com>
2023-03-07 14:01:27 -0300

    fix change database configs to postgres

--- BiblioteKa/settings.py
+++ BiblioteKa/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = "BiblioteKa.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+  "default": {
+      "ENGINE": "django.db.backends.postgresql",
+      "NAME": os.getenv("POSTGRES_DB_NAME"),
+      "USER": os.getenv("POSTGRES_USERNAME"),
+      "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+      "HOST": os.getenv("POSTGRES_DB_HOST"),
+      "PORT": os.getenv("POSTGRES_PORT"),
+  }
 }
 
 
