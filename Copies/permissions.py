5b705b03f09952357b39528e72e5e48358a18020
Amanda <amanda.costa1301@hotmail.com>
2023-03-07 13:34:15 -0300

    starting views.py and fixing models

---
+++ Copies/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+from rest_framework.views import Request, View
+
+
+class IsAdminOrAccountOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        elif request.user.is_authenticated and request.user.is_librarian:
+            return True
+
+        else:
+            return False
