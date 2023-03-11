669e7f0e391d55805bf75141cd9369f8b57b2ea1
Igor Ramon <ramon.riotinto@gmail.com>
2023-03-11 16:48:19 -0300

    resolving errors routes

--- Copies/urls.py
+++ Copies/urls.py
@@ -3,7 +3,7 @@ from .views import CopyView, CopyDetailView, BorrowView, BorrowReturn
 
 urlpatterns = [
     path("copies/", CopyView.as_view()),
-    path("copies/<int:copie_id>/", CopyDetailView.as_view()),
+    path("copies/<int:book_id>/", CopyDetailView.as_view()),
     path("borrow/<int:copie_id>/<int:user_id>/", BorrowView.as_view()),
     path("borrow/return/<int:copie_id>/<int:user_id>/", BorrowReturn.as_view()),
 ]
